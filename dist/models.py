6637d5af7463726e3f4d085ece59d8a7979f4bdd
Tchoops <dejan.samardzic133@gmail.com>
2022-06-10 23:23:43 +0200

    add navbar and content

--- dist/models.py
+++ dist/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    ID = models.CharField(max_length=20)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
